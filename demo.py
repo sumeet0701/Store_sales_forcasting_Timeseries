b311525124e851184ad950d9fa62619153a8f5bc
sumeet <maheshwarisumeet128@gmail.com>
2023-07-15 10:58:20 +0530

    Demo file created.

---
+++ demo.py
@@ -0,0 +1,16 @@
+from store_sales.pipeline.train import Pipeline
+from store_sales.logger import logging
+import os
+
+def main():
+    try:
+        pipeline = Pipeline()
+        pipeline.run_pipeline()
+
+    except Exception as e:
+            logging.error(f"{e}")
+            print(e)
+
+
+if __name__ == "__main__":
+     main()
